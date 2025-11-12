bec4fa6b593a148598b9e535ab57f289fa7ef81c
Anubhav Omar <anubhavomar123@gmail.com>
2025-11-12 20:16:25 +0530

    LeetCode problem 242 solution

---
+++ 242_Valid_Anagram.cpp
@@ -0,0 +1,35 @@
+// Solution in CPP
+// Approach -1 
+#include<string>
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         sort(s.begin(),s.end());
+         sort(t.begin(),t.end());
+        if(s==t) {
+            return true ;
+        }
+        else {
+            return false;
+        }
+    }   
+};
+
+//Approach - 2 
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        map<char,int> m1; 
+        map<char,int> m2 ; 
+        for(auto a :s ){
+            m1[a]++;
+        }
+        for(auto a :t ){
+            m2[a]++ ;
+        }
+        if(m1==m2){
+            return true ;
+        }
+        else return false ;
+    }
+};
