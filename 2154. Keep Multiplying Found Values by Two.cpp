c0258bb5d4cfd98b2bbde17870232ca4d48c99fa
Anubhav Omar <anubhavomar123@gmail.com>
2025-11-19 10:32:55 +0530

    Solution of LeetCode Problem

---
+++ 2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,25 @@
+// Solution of LeetCode Problem
+// 2154. Keep Multiplying Found Values by Two
+
+// Approach 1 (Using unordered map)
+
+
+class Solution
+{
+public:
+    int findFinalValue(vector<int> &nums, int original)
+    {
+        unordered_map<int, int> m;
+        for (int a : nums)
+        {
+            m[a]++;
+        }
+        int double_original = original;
+        while (m.find(double_original) != m.end())
+        {
+            double_original *= 2;
+        }
+
+        return double_original;
+    }
+};
