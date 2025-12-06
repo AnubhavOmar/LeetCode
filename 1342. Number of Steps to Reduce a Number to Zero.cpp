da837b5f1978b2fa8a90bccf2b987d5e5f52acf3
Anubhav Omar <anubhavomar123@gmail.com>
2025-12-06 17:33:25 +0530

    Solution of LeetCode Problem

---
+++ 1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,20 @@
+// Solution of LeetCode Problem
+// 1342. Number of Steps to Reduce a Number to Zero 
+// Solution in CPP 
+
+// Approach - 1 
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+       if(num == 0 ) return 0 ; 
+       int count =0 ; 
+       while (num != 0)
+       {
+        if(num % 2 ==0)  num = num /2 ;
+        else num -= 1 ;
+        count++;
+       }
+       return count ;
+    }
+};
