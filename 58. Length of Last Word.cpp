e65f727f49dec07008b51d3231e071017bb58f47
Anubhav Omar <anubhavomar123@gmail.com>
2025-12-30 22:41:03 +0530

    Solution of LeetCode Problem

--- 58. Length of Last Word.cpp
+++ 58. Length of Last Word.cpp
@@ -23,3 +23,29 @@ public:
       return count ;  
     }
 };
+
+//  Appraoch - 2 (Using Extra loop to eliminate a conditon from while Loop)
+
+class Solution {
+public:
+    int lengthOfLastWord(string s) 
+    {
+      int count= 0 ;
+      int i = s.size()-1;
+      for(int j = i ;j >=0;j--)
+      {
+        if(s[j] != ' ')
+        {
+            break ;
+        }
+        else i-- ;
+      }
+      while(i>=0 && s[i]!=' ' )
+      {
+        count++;
+        i--;
+      } 
+      
+      return count ;  
+    }
+};
