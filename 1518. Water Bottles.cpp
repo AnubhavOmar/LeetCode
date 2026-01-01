15672ba94e6d61a77b4a17d80dd1f138b6a229a7
Anubhav Omar <anubhavomar123@gmail.com>
2026-01-01 23:10:23 +0530

    Solution of LeetCode Problem

---
+++ 1518. Water Bottles.cpp
@@ -0,0 +1,26 @@
+// Solution of LeetCode Problem 
+// 1518. Water Bottles 
+// Solution in CPP 
+
+// Approach - 1 (Brute Force) 
+
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int drank = 0;
+        int emptybottles = 0;
+        while (numBottles > 0) {
+            numBottles--;
+            drank++;
+            emptybottles++;
+            if (emptybottles == numExchange) {
+                numBottles++;
+                emptybottles = 0;
+            }
+        }
+        return drank;
+    }
+};
+
+// Approach - 2 (Optimal Approach) 
+
