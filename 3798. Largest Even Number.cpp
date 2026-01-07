b5e07ad214ca1588d8c9e3ad674fbc3ff6dddcbc
Anubhav Omar <anubhavomar123@gmail.com>
2026-01-07 23:16:14 +0530

    Solution of LeetCode Problem

--- 3798. Largest Even Number.cpp
+++ 3798. Largest Even Number.cpp
@@ -28,4 +28,29 @@ public:
     }
 };
 
-// same Approach 1 but make only rem
+/*
+// same Approach 1 but make only removing the one digit variable 
+class Solution {
+public:
+    string largestEven(string s) {
+        int n = s.size() ;
+        int idx = -1 ;
+        for(int i = n -1 ; i>=0;i--)
+        {
+            if((s[i] - '0')%2==0)
+            {
+                idx = i;
+                break ;
+            }
+        }
+        string ans = "";
+        for(int i = 0 ;i<=idx;i++)
+        {
+            ans += s[i];
+        }
+        return ans;
+
+    }
+};
+
+*/
