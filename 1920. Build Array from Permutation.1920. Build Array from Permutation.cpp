a314d3c0c213f3b8ab5cdee3856cfe1a12a9d436
Anubhav Omar <anubhavomar123@gmail.com>
2026-01-16 21:02:45 +0530

    Solution of LeetCode Problem

---
+++ 1920. Build Array from Permutation.1920. Build Array from Permutation.cpp
@@ -0,0 +1,17 @@
+// Solution of LeetCode Problem 
+// 1920. Build Array from Permutation
+// Solution in CPP 
+
+// Approach - 1 ( using brute force approach with O(n) space )
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans ;
+        for(int i = 0 ; i<nums.size();i++)
+        {
+            ans.push_back(nums[nums[i]]);
+        }
+        return ans ;
+    }
+};
