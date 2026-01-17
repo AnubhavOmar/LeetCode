56c14a73ed8ac3ccb35c6110ee044e6422496570
Anubhav Omar <anubhavomar123@gmail.com>
2026-01-17 19:16:18 +0530

    Solution of LeetCode Problem

---
+++ 278. First Bad Version.c
@@ -0,0 +1,23 @@
+// Solution of LeetCode Problem 
+// 278. First Bad Version
+// Solution in C 
+
+// Approach - 1 ( Using Binary Search )
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+int firstBadVersion(int n) {
+    int i = 1, j = n;
+
+    while (i <= j) {
+        int mid = i + (j - i) / 2;
+
+        if (isBadVersion(mid)) {
+            j = mid - 1;
+        } else {
+            i = mid + 1;
+        }
+    }
+    return i;
+}
