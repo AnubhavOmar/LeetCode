35ee11d21d05b6f50b5713ab36a4d98e0174751f
Anubhav Omar <anubhavomar123@gmail.com>
2026-01-30 14:00:43 +0530

    Solution of LeetCode Problem

--- 42. Trapping Rain Water.cpp
+++ 42. Trapping Rain Water.cpp
@@ -76,3 +76,47 @@ public:
         return ans;
     }
 };
+
+// Appraoch - 3 Optimal Appraoch 
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0;
+        int r = height.size() - 1;
+
+        int lmax = 0;
+        int rmax = 0;
+
+        int ans = 0;
+
+        while (l < r) {
+            if (height[l] <= height[r])
+            { // left side
+                if (lmax > height[l])
+                {
+                    ans += lmax - height[l]; 
+                }
+                else
+                {
+                    lmax = height[l];
+                }
+                l++; 
+            }
+            else
+            { // right side
+                if (rmax > height[r])
+                {
+                    ans += rmax - height[r]; 
+                } 
+                else
+                {
+                    rmax = height[r];
+                }
+                r--; 
+            }
+        }
+
+        return ans;
+    }
+};
