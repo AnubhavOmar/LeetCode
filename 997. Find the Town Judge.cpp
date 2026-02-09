456d3f7b0a80396f1d4a7339c2efffff8bb2ece1
Anubhav Omar <anubhavomar123@gmail.com>
2026-02-09 21:27:50 +0530

    More approach of 997 added

--- 997. Find the Town Judge.cpp
+++ 997. Find the Town Judge.cpp
@@ -2,6 +2,37 @@
 // 997. Find the Town Judge
 // Solution in CPP 
 
+// Appraoch 3 
+// Time complexity = O(n+k)
+// Space complexity = O(n)
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) 
+    {
+        if(n==1 && trust.size()==0) return 1 ;
+        unordered_map<int,int> mp1 ;
+        unordered_map<int,int> mp2 ;
+        for(vector<int>& t : trust)
+        {
+            mp1[t[0]]++; // truster 
+            mp2[t[1]]++; // trusted
+        }
+
+        for(auto a : mp2)
+        {  
+            auto it = mp1.find(a.first);
+            int out = (it == mp1.end() ? 0 : it->second);
+            if(a.second == n-1  && out == 0)
+            {
+                return a.first ;
+            }
+        }
+        return -1 ;
+    }
+};
+
+
 
 // Appraoch 2  
 // Time complexity = O(n+k)
