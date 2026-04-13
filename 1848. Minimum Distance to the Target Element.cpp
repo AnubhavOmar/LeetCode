466137a311fd60083b25ae45ccf8e91812b8a2f2
Anubhav Omar <anubhavomar123@gmail.com>
2026-04-13 12:28:41 +0530

    Solution of LeetCode POTD

---
+++ 1848. Minimum Distance to the Target Element.cpp
@@ -0,0 +1,20 @@
+// Solution of LeetCode POTD
+// 1848. Minimum Distance to the Target Element
+// Solution in CPP 
+
+// Approach - 1     
+
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = INT_MAX;
+        for(int i = 0 ; i<nums.size() ; i++)
+        {
+            if(nums[i] == target)
+            {
+                ans = min(ans ,abs(i-start));
+            }
+        }
+        return ans;        
+    }
+};
