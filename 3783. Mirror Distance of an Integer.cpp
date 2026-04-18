5e38a3d93b3629c116a9a023a3407df85d715b9b
Anubhav Omar <anubhavomar123@gmail.com>
2026-04-18 10:23:48 +0530

    Solution of LeetCode POTD

--- 3783. Mirror Distance of an Integer.cpp
+++ 3783. Mirror Distance of an Integer.cpp
@@ -2,20 +2,48 @@
 // 3783. Mirror Distance of an Integer
 
 
+// Approach - 2 (Using reverse function)
 
-// Approach - 1 
 
-
-class Solution {
+class Solution2 {
 public:
     int mirrorDistance(int n) {
-        int num = n ;
-        int reverse = 0 ;  
-        while(num !=0)
-        {
-            reverse = reverse *10 + num % 10 ;
-            num /= 10 ;
-        }
-        return (abs(reverse - n));
+        
+        return abs(n - reverse(n));
     }
+private : 
+    int reverse(int n)
+    {
+        int  reversed_number = 0 ; 
+        while(n%10==0)
+        {
+            n /= 10 ;
+        }
+
+        while(n!=0)
+        {
+            reversed_number = reversed_number * 10 + n % 10 ; 
+            n /= 10 ; 
+        }
+        return reversed_number ;
+    }
+};
+
+
+
+// Approach - 1 (Using String variable)
+
+class Solution1 {
+public:
+    int mirrorDistance(int n)
+    {
+        int number = n ;
+        while(number % 10 ==0)
+        {
+            number /= 10 ;
+        }
+        string s = to_string(number) ;
+        reverse(s.begin() , s.end()) ;
+        return abs(n - stoi(s));
+    }  
 };
