a99c35860824c2700f1a86c1fa18ba13922b4f04
Anubhav Omar <anubhavomar123@gmail.com>
2026-05-15 22:10:13 +0530

    Solution of LeetCode POTD

--- 153. Find Minimum in Rotated Sorted Array.cpp
+++ 153. Find Minimum in Rotated Sorted Array.cpp
@@ -31,3 +31,28 @@ public:
         return nums[0];
     }
 };
+
+
+// Appraoch - 3  (Using Binary Search )
+// Time complexity : O(logn)
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int i = 0 ; 
+        int j = nums.size() - 1;
+        while(i<j)
+        {
+            int mid = i + (j-i)/2;
+            if(nums[mid] < nums[j])
+            {
+                j = mid ;
+            }
+            else if (nums[mid] > nums[j])
+            {
+                i = mid + 1;
+            }
+        }  
+        return nums[i];
+    }
+};
