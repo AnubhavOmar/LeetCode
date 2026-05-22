2ecee9acf722acdd5ca3289f591da171807ce19b
Anubhav Omar <anubhavomar123@gmail.com>
2026-05-22 14:15:09 +0530

    3925 Update

--- 3925. Concatenate Array With Reverse.cpp
+++ 3925. Concatenate Array With Reverse.cpp
@@ -1,14 +1,16 @@
-// Solution of LeetCode Problem 
+// Solution of LeetCode Problem
 // 3925. Concatenate Array With Reverse
 // Solution in CPP
 
-// Approach - 1 
+// Approach - 1
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> concatWithReverse(vector<int>& nums) {
-        int n = nums.size() ;
-        for(int i = n - 1 ; i >= 0 ; i--)
+    vector<int> concatWithReverse(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = n - 1; i >= 0; i--)
         {
             nums.push_back(nums[i]);
         }
