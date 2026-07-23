40fa67bfd270ba22b3768864337c7a4480b6d4e8
Anubhav Omar <183286261+AnubhavOmar@users.noreply.github.com>
2026-07-23 14:44:10 +0530

    Solution of LeetCode Problem

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -1,23 +1,54 @@
-// Solution in CPP 
-// Approach - 2 (Optimal Approach)
-class Solution {
+// Solution of LeetCode Problem 
+// 136. Single Number
+// Solution in CPP  
+
+// Approach - 1 (Brute Force)
+class Solution1 {
 public:
     int singleNumber(vector<int>& nums) {
-       // optimal solution 
-       int ans = 0 ;
-       for(int i= 0 ; i<nums.size();i++)
-       {
-        ans = ans ^nums[i];
-       }
-       return ans ;
+        int n =  nums.size();
+        for(int i = 0 ; i < n ;i++)
+        {
+            bool found = false;
+            for(int j = 0 ; j < n ;j++)
+            {
+                if( i != j && nums[i] == nums[j] )
+                {
+                    found = true; 
+                    break; 
+                }
+            }
+            if(!found)
+            {
+                return nums[i];
+            }
+        }
+        return -1 ; // not reachable line ;
     }
 };
 
+// Approach - 2 (Sorting)
+class Solution2 {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size() ;
+        for(int i = 1 ; i < n ; i+=2)
+        {
+            if(nums[i-1] != nums[i])
+            {
+                return nums[i-1];
+            }
+        }
+        return nums[n-1];
+    }
+};
 
-// Appraoch - 1 (brute force method)  
-
-
- int n = nums.size();
+// Approach - 3 (Hash Map / Frequency Count)
+class Solution3 {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
         map<int,int>m;
         for(int a: nums)
         {
@@ -32,4 +63,44 @@ public:
             }
         }
         return ans ;
+    }
+};
 
+// Approach - 4 (Bit Manipulation - XOR)
+class Solution4 {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size() ;
+        for(int i = 0 ; i < n ; i++)
+        {
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+};
+
+// Approach - 5 (Hash Set)
+class Solution5 {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_set<int> sett ;
+        int n = nums.size() ;
+        for(int i = 0 ;i<n;i++)
+        {
+            if(sett.find(nums[i]) != sett.end())
+            {
+                sett.erase(nums[i]);
+            }
+            else
+            {
+                sett.insert(nums[i]);
+            }
+        } 
+        for(auto a : sett)
+        {
+            return a;
+        }       
+        return -1;
+    }
+};
