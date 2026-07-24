6d9605949c9cc732e5e2975b83672296cd997ac0
Anubhav Omar <183286261+AnubhavOmar@users.noreply.github.com>
2026-07-24 22:31:23 +0530

    Solution of Single Number

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -104,3 +104,18 @@ public:
         return -1;
     }
 };
+
+// Approach - 6 (Using Xor)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size() ;
+        int sum = 0 ;
+        int xorr = 0 ;
+        for(int i =  0 ;i < nums.size() ; i++)
+        {
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+    }
+};
