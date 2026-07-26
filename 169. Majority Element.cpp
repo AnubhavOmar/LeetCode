b7736764a28ebb8bacf8d9cbc1662b160dbd3dab
Anubhav Omar <183286261+AnubhavOmar@users.noreply.github.com>
2026-07-26 16:04:47 +0530

    Solution of Problem

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -1,4 +1,8 @@
+// Solution of LeetCode Problem
+// 169. Majority Element
 // Solution in CPP 
+
+// Approach - 1 
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -21,5 +25,59 @@ public:
             
         }
         return number; 
+    }
+};
+
+// Appraoch - 2 
+// Moore's Voting Algorithm
+// Idea: If an element appears more than n/2 times, it cannot be completely cancelled out
+// by all other elements. By increasing/decreasing a counter, the final candidate is the majority element.
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size() ; 
+        int count = 0; 
+        int element = 0; 
+        for(int i  = 0 ; i < n ; i++)
+        {
+            if(count == 0)
+            {
+                element = nums[i];
+                count++;
+            }
+            else if(nums[i] == element)
+            {
+                count++;
+            }
+            else 
+            {
+                count-- ;
+            }
+        }    
+
+
+/*
+        // this is only to verify that the element is present in array more than n/2 times
+
+        int count1 = 0;
+        for(int i = 0 ;  i < n ; i++)
+        {
+            if(nums[i] == element)
+            {
+                count1++;
+            }
+        }
+
+        if(count1 >= n/2 )
+        {
+            return element ;
+        }
+        
+        return -1; // unreachable state
+*/
+
+    return element;
+
     }
 };
