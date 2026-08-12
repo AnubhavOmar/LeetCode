568848c0e017f74bef2472dd6251220d69057dc1
Anubhav Omar <183286261+AnubhavOmar@users.noreply.github.com>
2026-08-13 03:15:15 +0530

    solution of leetcode problem 94 and 129

--- 129. Sum Root to Leaf Numbers.cpp
+++ 129. Sum Root to Leaf Numbers.cpp
@@ -14,7 +14,140 @@
  * };
  */
 
-// Approach - 1 (Using Recursion)
+// Approach - 3
+// Using Morris Traversal
+// Time Complexity: O(2N) - Traversing each node and finding predecessors in the binary tree of size N
+// Space Complexity: O(1) - Constant auxiliary space used
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+
+        int sum = 0 ;
+        int current_number = 0 ;
+
+        while(root != NULL)
+        {
+            if(root->left == NULL)
+            {
+                // if root has the left child so update current_number
+                current_number = current_number * 10 + root->val ;
+                if(root->right == NULL)
+                {
+                    // and if the left node is the leaf node so add the current_numebr to the sum 
+                    sum += current_number;
+                }
+                root = root->right ;
+            }
+            else
+            {
+                // if root have left child so find predecessor 
+                TreeNode * pred = root->left ;
+                
+                int steps = 1 ;
+
+                while(pred->right != NULL && pred->right != root)
+                {
+                    pred = pred->right;
+                    // and also count the depth of the tree which we will further use for resetting the current_number ;
+                    steps++ ;
+                }
+                if(pred->right == NULL)
+                {
+                    // means we have not visited the left subtree yet 
+                    pred->right = root ;
+                    // update the current number
+                    current_number = current_number * 10 + root->val ;
+                    // so moving the root or curr node to left subtree
+                    root = root->left ;
+
+                }
+                else
+                {
+                    // if the left subtree is already visited so we have perform 4 tasks here 
+                    /*
+                    1.it means that we have the root to leaf path so first we add the current node value to  current number ans then we  can add current_number it in to our sum 
+                    2. reset the pred->right to null
+                    3. reset the current_number by dividing it steps times
+                    4. move the root node or current node to right side
+                    */
+                    if(pred->left == NULL)
+                    {
+                        sum += current_number ;
+                    }
+                    pred->right  = NULL ;
+                    while(steps != 0)
+                    {
+                        current_number /= 10 ;
+                        steps--;
+                    } 
+                    root = root->right;
+                }
+            }
+        }
+        return sum ;
+    }
+};
+
+
+// Approach - 2
+// Iterative approach using stack
+// Time Complexity: O(N) - Visiting every node in the binary tree of size N once
+// Space Complexity: O(H) - Stack space proportional to the height H of the binary tree
+class Solution2 {
+public:
+    int sumNumbers(TreeNode* root) {
+        if(root == NULL)
+        {
+            return 0;
+        }
+
+        int total_sum = 0 ;
+
+        stack<pair<TreeNode * , int>> st;
+
+        st.push({root , 0 });
+
+        while(!st.empty())
+        {
+            // pair<TreeNode * , int> p = st.top() ;
+            auto p = st.top() ;
+            st.pop() ;
+
+            TreeNode * node = p.first ;
+            int Current_Number = p.second;
+
+            // updating the Current_Number
+
+            Current_Number = Current_Number * 10 + node->val ;
+    
+            if(node->left == NULL && node->right == NULL)
+            {
+                // you are leaf
+                total_sum += Current_Number;
+            }
+            else
+            {
+                if(node->left != NULL)
+                {
+                    st.push({node->left , Current_Number});
+                }
+                
+                if(node->right != NULL)
+                {
+                    st.push({node->right , Current_Number});
+                }
+
+            }
+
+        }
+        return total_sum ;
+    }
+};
+
+// Approach - 1
+// Using Recursion (DFS)
+// Time Complexity: O(N) - Visiting every node in the binary tree of size N once
+// Space Complexity: O(H) - Recursive call stack space proportional to the height H of the binary tree
 class Solution1 {
 public:
     int sumNumbers(TreeNode* root) {
@@ -44,5 +177,4 @@ public:
 
         return ;
     }
-
 };
--- 94. Binary Tree Inorder Traversal.cpp
+++ 94. Binary Tree Inorder Traversal.cpp
@@ -1,67 +1,53 @@
-
+// Solution of LeetCode Problem
 // 94. Binary Tree Inorder Traversal
 // Solution in CPP 
 
-// Appraoch - 1 Morris Algorithm  
-// time complexity = O(2n)
-// space complixity = O(1)
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Approach - 1
+// Using Morris Algorithm
+// Time Complexity: O(2N) - Visiting each node and its predecessor thread in the binary tree of size N
+// Space Complexity: O(1) - Constant auxiliary space used
 
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        TreeNode * curr = root ;
+        vector<int> ans ;
 
-        TreeNode* curr = root;
-        TreeNode* pred;
-        vector<int> ans;
-
-        while (curr != NULL) {
-
-            // This should be == NULL
-            if (curr->left == NULL) {
-
+        while(curr != NULL)
+        {  
+            // if the left child is null of current
+            if(curr->left == NULL)
+            {
+                // if curr left child is null so visiting the node amd then move the pointer to the right side of the node
                 ans.push_back(curr->val);
-                curr = curr->right;
+                curr = curr->right ;
             }
-
-            else {
-
-                // Predecessor only when left exists
-                pred = curr->left;  // find the predecessor 
-
-                while (pred->right != NULL && pred->right != curr) {
-                    pred = pred->right;
+            else
+            {
+                
+                TreeNode *predecessor = curr->left ;
+                // find predecessor and creating a tempraray link to the curr node 
+                while(predecessor->right != NULL && predecessor->right != curr)
+                {
+                    predecessor = predecessor->right;
                 }
-
-                if (pred->right == NULL) { // if not visited before 
-
-                    // establishing a link to go back to current node 
-                    pred->right = curr;
-                    curr = curr->left;
+                // after finding the predecessor i.e rigth lastnode of the tree and now creating a temproary link and also moving the currnt pointer to the left subtree of the tree
+                if(predecessor->right == NULL)
+                {
+                    // it means that left subtree is not visited
+                    predecessor->right = curr ;
+                    curr = curr->left; 
                 }
-
-                else {  // if already visited
-
-                    // restore the tree structure
-                    pred->right = NULL;
-
+                else
+                {
+                    // it tells that left subtree is already visited so do 3 works i.e 1. reset the pointer , 2. visit the current node , 3. move current ptr to right side or jump on right sub tree
+                    predecessor->right = NULL ;
                     ans.push_back(curr->val);
-                    curr = curr->right;
+                    curr = curr->right; 
                 }
             }
         }
+            return ans;
 
-        return ans;
     }
 };
