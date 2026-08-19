4ba2de043ab5a04dfa534127483aac1af6cd7e4c
Anubhav Omar <183286261+AnubhavOmar@users.noreply.github.com>
2026-08-20 00:20:43 +0530

    solution of leetcode problem 69

--- 69. Sqrt(x).cpp
+++ 69. Sqrt(x).cpp
@@ -1,11 +1,43 @@
 // Solution of LeetCode Problem 
 // 69. Sqrt(x)
+// Solution in CPP
  
-// Approach - 1 (Brute force)
+// Approach - 1
+// Using Built-in Function
+// Time Complexity: O(1) - Assuming constant time for optimized library sqrt implementation
+// Space Complexity: O(1) - Constant auxiliary space used
 
 class Solution {
 public:
     int mySqrt(int x) {
       return int (sqrt(x));  
+    }
+};
+
+// Approach - 2
+// Using Binary Search
+// Time Complexity: O(log X) - Binary search over the range [1, X]
+// Space Complexity: O(1) - Constant auxiliary space used
+
+class Solution2 {
+public:
+    int mySqrt(int x) {
+      int low = 1 ;
+      int high = x ;
+      int ans = 0;
+      while(low <= high)
+      {
+            int mid = low + (high - low) / 2 ;
+            if (1LL * mid * mid <= x)
+            {
+                ans = mid ;
+                low = mid + 1 ;
+            }
+            else
+            {
+                high = mid - 1 ;
+            }
+      } 
+      return ans;
     }
 };
