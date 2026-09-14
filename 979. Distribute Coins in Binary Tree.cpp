0193ad6e689c3831e59914707fa04cb0bc35007a
Anubhav Omar <183286261+AnubhavOmar@users.noreply.github.com>
2026-09-14 10:02:00 +0530

    solution of leetcode problem 979

--- 979. Distribute Coins in Binary Tree.cpp
+++ 979. Distribute Coins in Binary Tree.cpp
@@ -1,8 +1,11 @@
-// Solution of LeetCode Problem 
+// Solution of LeetCode Problem
 // 979. Distribute Coins in Binary Tree
-// Solution in CPP 
+// Solution in CPP
 
-// Approach - 1 (Using DFS and post order traversal)
+// Approach - 1
+// Using Depth-First Search (DFS) Post-Order Traversal with Member Variable
+// Time Complexity: O(N) - Visiting every node in the binary tree of size N once
+// Space Complexity: O(H) - Recursive call stack space proportional to the height H of the binary tree
 
 /**
  * Definition for a binary tree node.
@@ -37,4 +40,32 @@ public:
         return extra_coin;
     }
 
+};
+
+// Approach - 2
+// Using Depth-First Search (DFS) Post-Order Traversal with Reference Parameter
+// Time Complexity: O(N) - Visiting every node in the binary tree of size N once
+// Space Complexity: O(H) - Recursive call stack space proportional to the height H of the binary tree
+
+class Solution {
+public:
+    int distributeCoins(TreeNode* root) {
+        int moves = 0 ;
+        dfs(root , 0 , moves );  
+        return moves ;
+    }
+    int dfs(TreeNode * node , int extra_coin , int & moves)
+    {
+        if(node == NULL)
+        {
+            return 0 ;
+        }
+        
+        int left_coin = dfs(node->left , extra_coin , moves);
+        int right_coin = dfs(node->right , extra_coin , moves) ;
+        
+        moves += abs(left_coin) + abs(right_coin) ;
+
+        return left_coin + right_coin + node->val - 1 ;
+    }
 };
